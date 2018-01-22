eeec7090e946ff94ade4dcec838e61b5e24697b9
Amanda Koster <amandakoster@gmail.com>
2018-01-22 10:16:19 -0800

    button

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -4,25 +4,29 @@ class SearchBar extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            term = ''
+            term : ''
         };
     }
 
-
-
-
+    onInputChange(e){
+        console.log(event.target.value)
+    }
 
     
     render(){
         return(
             <form className="input-group">
-            <input />
+                <input 
+                placeholder="Get a 5 day-forcast in your favorite cities"
+                className="form-control"
+                value={this.state.term}
+                onChange={this.onInputChange} />
             <span className="input-group-button">
-            <button type="submit" className="btn btn-secondary">Submit</button>
+                <button type="submit" className="btn btn-secondary">Submit</button>
             </span>
             </form>
 
-        )
+        );
     }
 }
 
