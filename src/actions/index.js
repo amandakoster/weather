5b0cf9ddfde1e8eac151d573d4f8ff6168334451
Amanda Koster <amandakoster@gmail.com>
2018-01-25 09:46:34 -0800

    adding sparklines

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,21 +1,17 @@
 import axios from 'axios';
 
 const API_KEY = '945b3d7fb367461654a9709c48d81389';
-// const API_URL = `appid=${API_KEY}`
 const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
+//action: function
 export function fetchWeather(city){
-// Create a URL
     const url = `${ROOT_URL}&q=${city},us`;
-
-//Make AJAX call
     const request = axios.get(url);
-
     // console.log('Request:', request)
 
-//Return a promise:
+//action: promise:
     return {
         type: FETCH_WEATHER,
         payload: request
--- src/containers/weather-list.js
+++ src/containers/weather-list.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine };
 
 class WeatherList extends React.Component {
 
     renderWeather(cityData){
         const name = cityData.city.name;
+        const temps = cityData.list.map(weather => weather.main.temp)
+        console.log('temps', temps)
+
         return(
             <tr key={name}>
                 <td>{name}</td>
+                <td>
+                <Sparklines height={120} width={180} data={temps}>
+                    <SparklinesLine color="green" />
+                </Sparklines>
+                </td>
             </tr>
         );
     }
