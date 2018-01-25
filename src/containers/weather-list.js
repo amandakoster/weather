2d6e423ba79507de41b90c047eb36889acf06adf
Amanda Koster <amandakoster@gmail.com>
2018-01-25 09:49:27 -0800

    charts are rendering

--- src/containers/weather-list.js
+++ src/containers/weather-list.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine };
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 class WeatherList extends React.Component {
 
